20ba066cce043600b06139897d8a67475e584416
ShantanuDaz <shaunofshan@gmail.com>
2024-09-07 11:57:05 +0530

    project setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+    open: true,
+  },
+  preview: {
+    port: 3000,
+    open: true,
+  },
   plugins: [react()],
-})
+});
