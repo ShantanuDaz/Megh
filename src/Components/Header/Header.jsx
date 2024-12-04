60130e797967ed4637f497e27b8fb95baafb1e0a
ShantanuDaz <shaunofshan@gmail.com>
2024-12-04 22:54:27 +0530

    added loading

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,27 +4,30 @@ import { useRef, useState } from "react";
 
 const Header = () => {
   const [isSearching, setIsSearching] = useState(false);
-  const [locations, setLocations] = useState();
+  const [locations, setLocations] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const snap = useSnapshot(state);
 
   const focus = (a) => a && a.focus();
 
-  const search = async (e) => {
+  const search = async (value) => {
     try {
-      if (e.target.value) setLocations([]);
+      if (value) setLocations([]);
       {
         const res = await fetch(
           `${
             import.meta.env.VITE_API_URL
           }locations/v1/cities/autocomplete?apikey=${
             import.meta.env.VITE_API_KEY
-          }&q=${e.target.value}`
+          }&q=${value}`
         );
         const data = await res.json();
         setLocations(data || []);
       }
     } catch (err) {
       console.log(err);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -68,12 +71,15 @@ const Header = () => {
             type="search"
             onBlur={() => setIsSearching(false)}
             ref={focus}
-            onChange={debouncedSearch}
+            onChange={(e) => {
+              debouncedSearch(e.target.value);
+              setIsLoading(true);
+            }}
             className="rounded-full px-3 py-2 w-[100%] sweep-in text-white bg-transparent border-2 outline-none"
           />
           <div className="absolute top-[100%] left-0 right-0 px-3 bg-white text-black max-h-[40vh] overflow-auto">
             {locations.map((location, i) => (
-              <div key={i}>
+              <div key={i} className="cursor-pointer hover:bg-gray-50">
                 <h5 className="text-xl">{location.LocalizedName}</h5>
                 <span className="text-xs">
                   {location.AdministrativeArea.LocalizedName},
